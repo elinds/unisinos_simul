3703197a695bc9ab3f81ff9ffd23b669fef4f729
ernesto lindstaedt <ernie.linds@gmail.com>
2021-03-22 14:47:14 -0300

    4th

---
+++ aula 4/simulador postogas cpp multithread/Carro.h
@@ -0,0 +1,12 @@
+#pragma once
+class Carro
+{
+	int placa;
+public:
+
+	Carro(int placa);
+	Carro();
+	~Carro();
+	void setPlaca(int placa);
+	int getPlaca();
+};
---
+++ aula 4/simulador postogas cpp multithread/Fila.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <queue>
+#include "Carro.h"
+#include <mutex>
+
+using namespace std;
+
+class Fila
+{
+	queue <Carro*> fila;
+	mutex queueMutex;
+
+public:
+	Fila();
+	~Fila();
+	void insert(Carro* c);
+	Carro* getFirst();
+	bool isEmpty();
+
+};
